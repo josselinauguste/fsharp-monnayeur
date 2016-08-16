ec6559b5524270929baab97fe87831ad183199b6
Josselin Auguste <jauguste@iblop.net>
2016-08-16 23:23:45 +0200

    Add use cases about free slots

--- src/Monnayeur/Monnayeur.fs
+++ src/Monnayeur/Monnayeur.fs
@@ -13,6 +13,7 @@ let ajoutePièce pièce (Monnayeur (début, pièces)) =
 let heureLimite (Monnayeur (début, pièces)) =
   let montant = pièces |> List.sum
   let tarifs = [(3, TimeSpan.FromHours 2.); (2, TimeSpan.FromHours 1.); (1, TimeSpan.FromMinutes 20.)]
+
   let rec durée montant (duréeAccumulée: TimeSpan) =
     let tarifPlusAvantageux = tarifs |> List.tryFind (fun (m, d) -> montant >= m)
     match tarifPlusAvantageux with
@@ -23,7 +24,27 @@ let heureLimite (Monnayeur (début, pièces)) =
     let débutPause = début.Date.AddHours 12.
     let finPause = début.Date.AddHours 14.
     if fin >= débutPause && début <= finPause then
-      durée + (finPause - débutPause)
+      let débutDécompte =
+        if début > débutPause then
+          début
+        else
+          débutPause
+      durée + (finPause - débutDécompte)
     else
       durée
-  durée montant TimeSpan.Zero |> compensePauseDeMidi début |> début.Add
+
+  let compenseNuit (début: DateTime) (durée: TimeSpan) =
+    let fin = début.Add(durée)
+    let débutNuit = début.Date.AddHours 18.
+    let duréeNuit = TimeSpan.FromHours(14.)
+    if fin >= débutNuit && début <= débutNuit + duréeNuit then
+      let débutDécompte =
+        if début > débutNuit then
+          début
+        else
+          débutNuit
+      durée + (débutNuit + duréeNuit - débutDécompte)
+    else
+      durée
+
+  durée montant TimeSpan.Zero |> compensePauseDeMidi début |> compenseNuit début |> début.Add
--- tests/Monnayeur.Tests/Monnayeur.Tests.fs
+++ tests/Monnayeur.Tests/Monnayeur.Tests.fs
@@ -25,3 +25,16 @@ let ``la pause de midi n'est pas décomptée`` () =
   let monnayeur = créer maintenant
   monnayeur |> ajoutePièce 1 |> ajoutePièce 2 |> heureLimite |> should equal limite
 
+[<Test>]
+let ``commencer le stationnement durant la pause de midi repousse le début`` () =
+  let maintenant = new DateTime(2016, 8, 1, 13, 0, 0)
+  let limite = maintenant.AddHours(3.)
+  let monnayeur = créer maintenant
+  monnayeur |> ajoutePièce 1 |> ajoutePièce 2 |> heureLimite |> should equal limite
+
+[<Test>]
+let ``la nuit n'est pas décomptée`` () =
+  let maintenant = new DateTime(2016, 8, 1, 17, 0, 0)
+  let limite = new DateTime(2016, 8, 2, 9, 0, 0)
+  let monnayeur = créer maintenant
+  monnayeur |> ajoutePièce 1 |> ajoutePièce 2 |> heureLimite |> should equal limite
